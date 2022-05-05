b4d85899cbb5577177f5244ad682172487450367
Rogachev_vv <vlad2320082009@gmail.com>
2022-05-05 14:52:19 +0400

    Пробую сделать удаление картинок из заметок

--- app/src/main/java/com/example/shoppinglist/adapter/ImageAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/adapter/ImageAdapter.kt
@@ -3,15 +3,17 @@ package com.example.shoppinglist.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 
-class ImageAdapter(private val names: MutableList<String>) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+class ImageAdapter(private val listener: Listener, private val names: MutableList<String>) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var imageView: ImageView = itemView.findViewById(R.id.imViewImage)
+        var delete:Button = itemView.findViewById(R.id.imClose)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,7 +24,19 @@ class ImageAdapter(private val names: MutableList<String>) : RecyclerView.Adapte
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.imageView.setImageURI(names[position].toUri())
+        holder.imageView.setOnClickListener {
+            listener.onClickItem(position)
+        }
+        holder.delete.setOnClickListener {
+            listener.deleteItem(position)
+        }
+
     }
 
     override fun getItemCount() = names.size
+
+    interface Listener {
+        fun deleteItem(id: Int)
+        fun onClickItem(position: Int)
+    }
 }
